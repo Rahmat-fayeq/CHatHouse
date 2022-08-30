513662b74f13e614c11b435d549e811f86086637
Hulk <hulk@yahoo.com>
2022-08-30 14:12:43 +0430

    6th commit

--- studybud/settings.py
+++ studybud/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,7 +126,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'images/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG:
+    STATIC_ROOT = ''
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
